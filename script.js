c38633d253474de5dbea324b87c462c73ae66805
Peterson Sales <petersonsalesdev@gmail.com>
2024-05-26 13:57:30 -0300

    commit feito

---
+++ script.js
@@ -0,0 +1,20 @@
+function togglomode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector('#profile img ')
+      
+  // substituir a imagem
+  if(html.classList.contains('light')) {
+    // se tiver liga a light, adiciona a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'foto do peterson sales sorrindo')
+  } else {
+      // se tiver sem light mode, manter a imagem normal 
+      img.setAttribute('src', './assets/avatar.png')
+      img.setAttribute('alt', 'Foto do Peterson mais novo')
+  } 
+  
+
+}
